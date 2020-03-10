8340bb4b8f533e2f1bf75ca7a501be3c21c696b7
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-03-10 18:31:30 +0100

    add fun to process a list of requests

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/wsd/WSDClient.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/wsd/WSDClient.kt
@@ -19,28 +19,66 @@ class WSDClient(
     val host: String = "localhost",
     val port: String = "5000"
 ) {
+    init {
+        System.err.close();
+        System.setErr(System.out);
+    }
+
+    fun disambiguate(requests: List<WSDRequest>, threshold: Double = 0.5) =
+        requests
+            .map { it.convertToCsvString() }
+            .joinToString("\n")
+            .let { sendRequest(it, requests.map { it.wordSenses }.flatten()) }
+            .let { it?.cut(requests.map { it.wordSenses.size }) }
+            ?.map { response ->
+                response
+                    .filter { (_, score) -> score >= threshold }
+                    .map { (sense, _) -> sense }
+            }
+
     fun disambiguate(request: WSDRequest, threshold: Double = 0.5) =
-        post(url = "http://$host:$port", data = request.convertToCsvString()).text
-            .let { readResponse(it, request) }
+        request.convertToCsvString()
+            .let { sendRequest(it, request.wordSenses) }
             ?.filter { (_, score) -> score >= threshold }
             ?.map { (sense, _) -> sense }
 
-    internal fun readResponse(rawResponse: String, request: WSDRequest): WSDResponse? {
-        return rawResponse.split("\n")
+    internal fun sendRequest(rawRequest: String, wordSenses: List<WordSense>): WSDResponse? {
+        return post(url = "http://$host:$port", data = rawRequest)
+            .text
+            .split("\n")
             .map { it.split("\t") }
             .also { lines -> if (!lines.all { it.size == 3 }) return null }
             .map { it[2].toDouble() }
-            .let { request.wordSenses.zip(it) }
+            .let { wordSenses.zip(it) }
     }
+
+    internal fun String.markTarget(index: Int) =
+        this.split("""\s+""".toRegex()).let {
+            it.take(index)
+                .plus("\"")
+                .plus(it[index])
+                .plus("\"")
+                .plus(it.drop(index + 1))
+                .joinToString(" ")
+        }
 
     internal fun WSDRequest.convertToCsvString() =
         this.wordSenses.map {
             listOf(
                 "id",
-                "?",
-                this.sentence,
+                "0",
+                this.sentence.markTarget(this.targetIndex),
                 it.gloss,
                 it.senseKey
             ).joinToString("\t")
         }.joinToString("\n")
+
+    internal fun WSDResponse.cut(sizes: List<Int>): List<WSDResponse> {
+        var responseList = this
+        return sizes.map {
+            val thisResponses = responseList.take(it)
+            responseList = responseList.drop(it)
+            thisResponses
+        }
+    }
 }
