788da087fd82224ff0a29bbe1e5dd205e9af52eb
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-03-11 11:02:57 +0100

    init decomposition

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ARCSolver.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ARCSolver.kt
@@ -1,8 +1,28 @@
 package arc
 
+import de.kimanufaktur.nsm.decomposition.Decomposition
+import de.kimanufaktur.nsm.decomposition.Definition
+import de.kimanufaktur.nsm.decomposition.graph.conceptCache.GraphUtil
+import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.MarkerPassingConfig
+
 class ARCSolver() {
+    internal val decomposition = Decomposition()
 
     fun invoke(task: ArcTask): Label {
+        val graphs = listOf(
+            task.warrant0,
+            task.warrant1,
+            task.reason,
+            task.claim,
+            task.debateTitle,
+            task.debateInfo
+        )
+            .map { str ->
+                val def = Definition(str)
+                def.definition.map { word ->
+                    GraphUtil.getGraph(word.litheral, word.wordType, MarkerPassingConfig.getDecompositionDepth())
+                }
+            }
         return Label.UNKNOWN
     }
 }
