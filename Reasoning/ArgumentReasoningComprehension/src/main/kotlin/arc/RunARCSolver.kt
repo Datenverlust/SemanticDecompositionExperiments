af690da80167bf7c17c8360bf3fc7f42ff3e89d3
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-03-11 10:32:55 +0100

    print score

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/RunARCSolver.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/RunARCSolver.kt
@@ -6,13 +6,9 @@ import arc.utils.readDataset
 fun main() {
     ARCSolver().let { solver ->
         readDataset(Dataset.ADVERSIAL_TRAIN)?.let { dataset ->
-            dataset
-                .map { task ->
-                    task to solver.invoke(task)
-                }
-                .forEach { (task, result) ->
-
-                }
+            dataset.filter { it.correctLabelW0orW1 == solver.invoke(it) }
+                .let { (it.size.toDouble() / dataset.size.toDouble()) * 100 }
+                .let { println("$it  % richtig gelabelt") }
         }
     }
 }
