b3aa9097d23fe7e0bff3fe55c5ccaa8be84ef045
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-03-11 09:48:02 +0100

    move sandbox to wsdsandbox

---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/wsd/WSDSandbox.kt
@@ -0,0 +1,22 @@
+package arc.wsd
+
+import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+
+fun main(){
+        {}::class.java.getResourceAsStream("wsd_examples.yml").readBytes().let { bytes ->
+        val client = WSDClient()
+        ObjectMapper(YAMLFactory()).registerModule(KotlinModule() as Module?)
+            .readValue<List<WSDRequest>>(bytes)
+            .let { requests -> client.disambiguate(requests)?.let { requests.zip(it) } }
+            ?.forEach { (request, senses) ->
+                println("sentence: ${request.sentence}")
+                println("word to disambiguate: ${request.sentence.split("""\s+""".toRegex())[request.targetIndex]}")
+                println("matching sense: ${senses.map{it.gloss+ " - " + it.senseKey}.joinToString(" & ")}")
+                println()
+            }
+    }
+}
