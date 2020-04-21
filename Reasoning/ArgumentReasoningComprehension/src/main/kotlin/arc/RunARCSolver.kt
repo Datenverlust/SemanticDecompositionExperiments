bc6877ae1b3ce6297d4f1dd953f0e6a2c72c781a
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-04-21 20:48:34 +0200

    add value sizeToTake

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/RunARCSolver.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/RunARCSolver.kt
@@ -1,22 +1,27 @@
 package arc
 
-import arc.utils.Dataset
-import arc.utils.readDataset
+import arc.util.Dataset
+import arc.util.printProgress
+import arc.util.readDataset
 
 fun main() {
     ARCSolver().let { solver ->
         readDataset(Dataset.ADVERSIAL_TRAIN)?.let { dataset ->
-            dataset
-                .take(6)
+            val sizeToTake = 10
+            dataset.asSequence()
+                .take(sizeToTake)
+                .printProgress(1, sizeToTake)
                 .mapIndexed { index, task ->
                     val solution = solver.invoke(task)
-                    println("#${index + 1} - ID:${task.id}")
                     if (task.correctLabelW0orW1 == solution) 1.0
                     else 0.0
                 }
                 .average()
-                .let { it * 100 }
-                .let { println("$it  % richtig gelabelt") }
+                .let { result ->
+                    "%s richtig gelabelt".format(
+                        String.format("%.2f%%", result * 100)
+                    ).let { println(it) }
+                }
         }
     }
 }
