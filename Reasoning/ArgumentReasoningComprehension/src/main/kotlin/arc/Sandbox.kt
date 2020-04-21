350f6ddd3b4722c8536fdce7b3daf2b7d46316b1
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-04-21 20:49:33 +0200

    try xml mapper in sandbox script

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/Sandbox.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/Sandbox.kt
@@ -1,7 +1,13 @@
 package arc
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import de.kimanufaktur.nsm.decomposition.Decomposition
 
+data class Wrapper(
+    val id: String
+)
 fun main() {
-    val decomposition = Decomposition()
+    val xmlMapped = "<wrapper id=\"identification\"></wrapper>".let { XmlMapper().readValue<Wrapper>(it) }
+    println("nothing")
 }
