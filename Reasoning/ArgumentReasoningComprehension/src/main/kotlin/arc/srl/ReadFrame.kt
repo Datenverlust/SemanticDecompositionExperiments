33d16064a5032ad23b5a064a91d7228ca3910a36
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-04-21 20:50:02 +0200

    Add new kotlin frame reader

---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/srl/ReadFrame.kt
@@ -0,0 +1,62 @@
+package arc.srl
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.io.File
+
+internal val kotlinXmlMapper = XmlMapper(JacksonXmlModule().apply {
+    setDefaultUseWrapper(false)
+}).registerKotlinModule()
+    .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+internal data class Frameset(
+    val predicate: Predicate? = null
+)
+
+internal data class Predicate(
+    val lemma: String? = null,
+    val roleset: List<Roleset>? = null
+)
+
+internal data class Roleset(
+    val id: String? = null,
+    val name: String? = null,
+    val roles: Roles? = null
+)
+
+internal data class Roles(
+    val role: List<Role>? = null
+)
+
+internal data class Role(
+    val descr: String? = null,
+    val f: String? = null,
+    val n: String? = null,
+    val vnRole: VnRole? = null
+)
+
+internal data class VnRole(
+    val vntheta: String? = null
+)
+
+internal fun Frameset.toRoleMap() =
+    this.predicate?.roleset
+        ?.mapNotNull { roleset ->
+            roleset.id?.let { id ->
+                roleset.roles?.role
+                    ?.map { it.descr + it.vnRole?.vntheta }
+                    ?.let { roles -> id to roles }
+            }
+        }
+        ?.toMap()
+
+internal val framesDir = File({}::class.java.getResource("").path, "frames")
+
+fun getRoleset(verb: String) = File(framesDir, "$verb.xml")
+    .readText()
+    .let { kotlinXmlMapper.readValue<Frameset>(it).toRoleMap() }
