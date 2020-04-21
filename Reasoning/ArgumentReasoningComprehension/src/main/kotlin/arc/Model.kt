e63ef9510d193823b7933422f790f93048c935cd
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-04-21 20:45:09 +0200

    refactor label in Model

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/Model.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/Model.kt
@@ -4,17 +4,14 @@ data class ArcTask(
     val id: String,
     val warrant0: String,
     val warrant1: String,
-    val correctLabelW0orW1: Label,
+    val correctLabelW0orW1: ArcLabel,
     val reason: String,
     val claim: String,
     val debateTitle: String,
     val debateInfo: String
 )
 
-fun ArcTask.getConceptElements() =
-    listOf(this.warrant0, this.warrant1, this.reason, this.claim, this.debateTitle, this.debateInfo)
-
-enum class Label {
+enum class ArcLabel {
     W0,
     W1,
     UNKNOWN
