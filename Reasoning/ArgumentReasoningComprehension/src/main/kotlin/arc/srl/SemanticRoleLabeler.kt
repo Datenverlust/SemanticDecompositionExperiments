6c0e073d7ff656338ec6c92e4ad55d3b2b582aae
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-04-22 15:31:36 +0200

    replace srl invoke with kotlin style fun

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/srl/SemanticRoleLabeler.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/srl/SemanticRoleLabeler.kt
@@ -1,18 +1,11 @@
 package arc.srl
 
 import arc.util.userHome
-import de.kimanufaktur.nsm.decomposition.Concept
-import de.kimanufaktur.nsm.decomposition.Decomposition
 import de.kimanufaktur.nsm.decompostion.Dictionaries.DictUtil
+import edu.stanford.nlp.ling.CoreLabel
 import se.lth.cs.srl.CompletePipeline
-import se.lth.cs.srl.corpus.Sentence
-import se.lth.cs.srl.corpus.Word
 import se.lth.cs.srl.options.CompletePipelineCMDLineOptions
 import java.io.File
-import java.util.*
-import kotlin.Comparator
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class SemanticRoleLabeler() {
     internal val language = "eng"
@@ -30,7 +23,7 @@ class SemanticRoleLabeler() {
             taggerPath,
             "-parser",
             parserPath,
-            "srl",
+            "-srl",
             srlPath
         )
     )
@@ -51,83 +44,25 @@ class SemanticRoleLabeler() {
             .also { it.parseCmdLineArgs(pipelineOptions) }
             .let { CompletePipeline.getCompletePipeline(it) }
 
-    fun invoke(text: List<List<String>>): MutableMap<List<Concept>, List<String>> {
-        val roleMap: MutableMap<List<Concept>, List<String>> = HashMap()
-        //Für jeden im Schema vorkommenden Satz
-        for (i in text.indices) {
-            val sentence = text[i]
-            val newText: MutableList<String?> = ArrayList()
-            newText.add("<root>")
-            newText.addAll(sentence)
-            var s: Sentence? = null
-            try {
-                s = pipeline.parse(newText)
-            } catch (e: Exception) {
-                e.printStackTrace()
+    fun invoke(tokenizedSentence: List<CoreLabel>) =
+        pipeline.parse(
+            tokenizedSentence.map { token ->
+                token.originalText()
             }
-            val roleList: Map<String, Map<List<Concept>, List<String>>> = HashMap()
-
-            //Für jedes im Satz vorkommende Prädikat
-            for (p in s!!.predicates) {
-
-                //Hole Framset für Prädikat
-                val newRoleSet = getRoleset(p.sense)
-                if (newRoleSet != null && newRoleSet.size != 0) { //Wenn Verb in frames existiert
-
-                    //Für jedes gefundene Argument von Prädikat
-                    for (arg in p.argMap.keys) {
-                        val argName = getLongName(p.argMap[arg])
-                        var argList = newRoleSet[argName]
-                        val thisRoles: MutableList<Concept> = ArrayList()
-                        for (w in getFull(arg)) {
-                            var con: Concept? = null
-                            if (!Decomposition.getConcepts2Ignore().contains(w.lemma)) {
-                                con = if (w.lemma == w.form.toLowerCase()) Concept(w.form) else Concept(w.lemma)
-                            }
-                            if (con != null) {
-                                thisRoles.add(con)
-                            }
+        )
+            .predicates.mapNotNull { predicate ->
+                getRoleset(predicate.lemma, predicate.sense)
+                    ?.let { roleset ->
+                        predicate.argMap.mapNotNull { (word, argName) ->
+                            roleset.getOrNull(argName.removePrefix("A").toInt().dec())
+                                ?.let { role ->
+                                    tokenizedSentence.get(word.idx) to role
+                                }
                         }
-                        if (argList == null) {
-                            argList = ArrayList()
-                            if (getModifier(argName) != null) getModifier(argName)?.let { argList.add(it) }
-                        }
-                        if (argList.size != 0) roleMap[thisRoles] = argList
+                            .toMap()
                     }
-                }
             }
-        }
-        return roleMap
-    }
-
-    private fun getFull(w: Word): Set<Word> {
-        val span: MutableSet<Word> = TreeSet(Comparator { o1, o2 -> if (o1.idx < o2.idx) -1 else if (o1.idx > o2.idx) 1 else 0 })
-        span.add(w)
-        val children: MutableList<Word> = LinkedList()
-        children.addAll(w.children)
-        while (!children.isEmpty()) {
-            val c = children.removeAt(0)
-            if (span.contains(c)) continue
-            span.add(c)
-            children.addAll(c.children)
-        }
-        return span
-    }
-
-    private fun getLongName(inputName: String?): String {
-        var name = inputName
-        if (Character.isDigit(name!![1])) {
-            name = name[0].toString() + "rg" + name[1]
-        }
-        return name
-    }
-
-    private fun getModifier(inputName: String?): String? {
-        var name = inputName
-        val nameParts = name!!.split("-".toRegex()).toTypedArray()
-        if (nameParts[0] == "AM") {
-            name = if (nameParts[1] == "CAU") "cause" else if (nameParts[1] == "LOC") "location" else if (nameParts[1] == "DIR") "direction" else if (nameParts[1] == "GOL") "goal" else if (nameParts[1] == "MNR") "manner" else if (nameParts[1] == "TMP") "time" else if (nameParts[1] == "EXT") "extent" else if (nameParts[1] == "PRP") "purpose" else if (nameParts[1] == "DIS") "discourse" else if (nameParts[1] == "CXN") "construction" else null
-        }
-        return name
-    }
+            .asSequence()
+            .flatMap { it.asSequence() }
+            .groupBy({ it.key }, { it.value })
 }
