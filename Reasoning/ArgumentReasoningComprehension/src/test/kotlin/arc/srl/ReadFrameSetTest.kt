4d2aed38ee2ce250a4b2add9b49e5f44edc186fe
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-07-24 11:50:40 +0200

    fix MismatchedInputException

--- Reasoning/ArgumentReasoningComprehension/src/test/kotlin/arc/srl/ReadFrameSetTest.kt
+++ Reasoning/ArgumentReasoningComprehension/src/test/kotlin/arc/srl/ReadFrameSetTest.kt
@@ -1,6 +1,5 @@
 package arc.srl
 
-import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.junit.Assert
 import org.junit.Test
 
@@ -12,23 +11,19 @@ class ReadFrameSetTest {
             ?.filterNot { it.name == "frameset.dtd" }
             ?.forEach {
                 val predicate = it.name.substringBefore(".xml")
-                try {
-                    Assert.assertNotNull("frameSet for lemma $predicate must exist", getFrameset(predicate))
-                } catch (e:MismatchedInputException){
-                    println(predicate)
-                }
+                Assert.assertNotNull("frameSet for lemma $predicate must exist", getFrameset(predicate))
             }
     }
 
     @Test
     fun testUnknownLemma() {
-        Assert.assertNull("unknown lemma must not exist", getRoleSet("unknown","0"))
+        Assert.assertNull("unknown lemma must not exist", getRoleSet("unknown", "0"))
     }
 
     @Test
     fun testUnknownSense() {
         val predicate = "play"
         Assert.assertNotNull("known sense play.0 must exist", getRoleSet(predicate, "$predicate.01"))
-        Assert.assertNull("unknown sense play.7 must not exist", getRoleSet(predicate,  "7"))
+        Assert.assertNull("unknown sense play.7 must not exist", getRoleSet(predicate, "7"))
     }
 }
