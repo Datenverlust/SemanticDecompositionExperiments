fe7974890a4ddf34b2b4aa073bf413c10f58551f
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-07-28 16:37:03 +0200

    refactor config

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcConfig.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcConfig.kt
@@ -1,24 +1,23 @@
 package arc
 
-import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.MarkerPassingConfig
+val decompositionDepth = 1
 
-val decompositionDepth = MarkerPassingConfig.getDecompositionDepth()
+val startActivation = 100
+val threshold = 0.1
 
-val startActivation = MarkerPassingConfig.getStartActivation()
-val threshold = MarkerPassingConfig.getThreshold()
+val synonymLinkWeight = 0.9
+val definitionLinkWeight = 0.8
+val antonymLinkWeight = -0.9
+val hyponymLinkWeight = 0.5
+val hypernymLinkWeight = 0.5
+val meronymLinkWeight = 0.5
+val syntaxLinkWeight = 0.8
+val namedEntityLinkWeight = 1.0
+val semanticRoleLinkWeight = 1.0
 
-val synonymLinkWeight = MarkerPassingConfig.getSynonymLinkWeight()
-val definitionLinkWeight = MarkerPassingConfig.getDefinitionLinkWeight()
-val antonymLinkWeight = MarkerPassingConfig.getAntonymLinkWeight()
-val meronymLinkWeight = MarkerPassingConfig.getMeronymLinkWeight()
-val hyponymLinkWeight = MarkerPassingConfig.getHyponymLinkWeight()
-val hypernymLinkWeight = MarkerPassingConfig.getHypernymLinkWeight()
-val syntaxLinkWeight = MarkerPassingConfig.getSyntaxLinkWeight()
-val namedEntityLinkWeight = MarkerPassingConfig.getNerLinkWeight()
-val semanticRoleLinkWeight = MarkerPassingConfig.getRoleLinkWeight()
-
-val activeSyntax = true
-val activeSrl = true
-val activeNer = true
-val activeWsd = true
-val activeNeg = true
+val useSemanticDecomposition = true
+val useSyntaxDependencies = true
+val useSemanticRoles = true
+val useNamedEntities = true
+val useWsd = true
+val useNegationHandling = true
