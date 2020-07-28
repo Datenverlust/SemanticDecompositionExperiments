297e21aa1553f3572c877037dfc278181493a0ee
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-07-28 18:58:02 +0200

    change start activation to double

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcConfig.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcConfig.kt
@@ -1,23 +1,23 @@
 package arc
 
-val decompositionDepth = 1
+var decompositionDepth = 1
 
-val startActivation = 100
-val threshold = 0.1
+var startActivation = 100.0
+var threshold = 0.1
 
-val synonymLinkWeight = 0.9
-val definitionLinkWeight = 0.8
-val antonymLinkWeight = -0.9
-val hyponymLinkWeight = 0.5
-val hypernymLinkWeight = 0.5
-val meronymLinkWeight = 0.5
-val syntaxLinkWeight = 0.8
-val namedEntityLinkWeight = 1.0
-val semanticRoleLinkWeight = 1.0
+var synonymLinkWeight = 0.9
+var definitionLinkWeight = 0.8
+var antonymLinkWeight = -0.9
+var hyponymLinkWeight = 0.5
+var hypernymLinkWeight = 0.5
+var meronymLinkWeight = 0.5
+var syntaxLinkWeight = 0.8
+var namedEntityLinkWeight = 1.0
+var semanticRoleLinkWeight = 1.0
 
-val useSemanticDecomposition = true
-val useSyntaxDependencies = true
-val useSemanticRoles = true
-val useNamedEntities = true
-val useWsd = true
-val useNegationHandling = true
+var useSemanticDecomposition = true
+var useSyntaxDependencies = true
+var useSemanticRoles = true
+var useNamedEntities = true
+var useWsd = true
+var useNegationHandling = true
