f0c51ec7318f2cb98bd9bc324cc8f785820a7176
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-07-30 21:15:15 +0200

    add stopwords to decomp utils

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/DecompositionUtil.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/DecompositionUtil.kt
@@ -6,3 +6,17 @@ import de.kimanufaktur.nsm.decomposition.Decomposition
 private val semanticDecomposition = Decomposition()
 
 fun Concept.decompose(depth: Int): Concept = semanticDecomposition.decompose(this.litheral, this.wordType, depth)
+
+private val stopWords = {}::class.java.getResourceAsStream("stopwords.txt").bufferedReader().readLines()
+
+fun Concept.isStopWord() = litheral in stopWords
+
+fun Concept.copy() = Concept(litheral).also {
+//    it.decompositionlevel = decompositionlevel
+    it.senseKeyToSynonymsMap = senseKeyToSynonymsMap
+    it.senseKeyToAntonymsMap = senseKeyToAntonymsMap
+    it.senseKeyToHyponymsMap = senseKeyToHyponymsMap
+    it.senseKeyToHypernymsMap = senseKeyToHypernymsMap
+    it.senseKeyToMeronymsMap = senseKeyToMeronymsMap
+    it.senseKeyToGlossMap = senseKeyToGlossMap
+}
