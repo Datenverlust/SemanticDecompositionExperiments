9a31cec280a959d377f9a7316b8efba0081dd685
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-08-05 14:38:18 +0200

    add negation resolver

---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/negation/ResolveNegation.kt
@@ -0,0 +1,35 @@
+package arc.negation
+
+import arc.ArcGraphConfig
+import arc.ifWsd
+import arc.util.copy
+import arc.util.decompose
+import arc.wsd.disambiguateBy
+import de.kimanufaktur.nsm.decomposition.Concept
+import edu.stanford.nlp.ling.CoreLabel
+import edu.stanford.nlp.semgraph.SemanticGraphEdge
+
+fun Concept.resolveNegation(
+    config: ArcGraphConfig,
+    coreLabel: CoreLabel,
+    markedContext: String,
+    negationEdges: List<SemanticGraphEdge>
+) =
+    if (coreLabel.isNegationTarget(negationEdges))
+        assignedSenseKeys.ifEmpty { definitions.map { it.sensekey} }
+            .mapNotNull { senseKey -> senseKeyToAntonymsMap[senseKey] }
+            .flatten().firstOrNull()
+            ?.decompose(config.depth)
+            ?.ifWsd(config) { disambiguateBy(markedContext) }
+            ?: pseudoAntonym()
+    else this
+
+
+private fun Concept.pseudoAntonym() = copy().also {
+    it.negated = true
+    it.senseKeyToSynonymsMap = senseKeyToAntonymsMap
+    it.senseKeyToAntonymsMap = senseKeyToSynonymsMap
+}
+
+fun CoreLabel.isNegationTarget(negationEdges: List<SemanticGraphEdge>) = negationEdges
+    .any { edge -> edge.target.backingLabel() == this }
