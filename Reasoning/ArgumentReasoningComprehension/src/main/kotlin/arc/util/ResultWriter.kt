8a8f826a118a0746bfd2022f2e90d66cfd41080c
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-08-19 13:25:36 +0200

    add ResultWriter.kt

---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/ResultWriter.kt
@@ -0,0 +1,12 @@
+package arc.util
+
+import java.io.File
+
+val resultsFile = {}::class.java.getResourceAsStream("results.txt")
+
+//fun getDoneTasks(): List<String> {
+//
+//}
+//
+//fun readResultsFile()
+//fun writeToResultsFile()
