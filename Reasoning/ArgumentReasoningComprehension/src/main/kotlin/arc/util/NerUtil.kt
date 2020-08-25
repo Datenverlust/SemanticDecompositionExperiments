c6c16796579d6cc9f69a525c7e09eecc33b94fca
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-08-25 17:50:23 +0200

    add fun to check ner confidence

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/NerUtil.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/NerUtil.kt
@@ -2,4 +2,8 @@ package arc.util
 
 import edu.stanford.nlp.ling.CoreLabel
 
-fun CoreLabel.isNamedEntity() = ner() != null && ner() != "O"
+fun CoreLabel.isNamedEntity() = ner() != null && ner() != "O" && nerConfidence().isReliable(0.5)
+
+private fun Map<String, Double>.isReliable(threshold: Double) =
+    get("O")?.let { noNerConfidence -> return noNerConfidence <= threshold }
+        ?: toList().first().second > threshold
