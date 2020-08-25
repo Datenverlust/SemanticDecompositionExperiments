4ed4d4457904a27cbda7b487b40361ffd4ef25bb
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-08-25 16:57:33 +0200

    save source to nodes

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcMarkerPassing.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcMarkerPassing.kt
@@ -1,19 +1,10 @@
 package arc
 
-import de.kimanufaktur.markerpassing.Link
+import arc.util.reverseByType
+import arc.util.toEmptyLink
 import de.kimanufaktur.nsm.decomposition.Concept
-import de.kimanufaktur.nsm.decomposition.graph.edges.EdgeType
 import de.kimanufaktur.nsm.decomposition.graph.edges.WeightedEdge
 import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.DoubleMarkerPassing
-import de.kimanufaktur.nsm.graph.entities.links.AntonymLink
-import de.kimanufaktur.nsm.graph.entities.links.DefinitionLink
-import de.kimanufaktur.nsm.graph.entities.links.HypernymLink
-import de.kimanufaktur.nsm.graph.entities.links.HyponymLink
-import de.kimanufaktur.nsm.graph.entities.links.MeronymLink
-import de.kimanufaktur.nsm.graph.entities.links.NamedEntityLink
-import de.kimanufaktur.nsm.graph.entities.links.SemanticRoleLink
-import de.kimanufaktur.nsm.graph.entities.links.SynonymLink
-import de.kimanufaktur.nsm.graph.entities.links.SyntaxLink
 import de.kimanufaktur.nsm.graph.entities.marker.DoubleMarkerWithOrigin
 import de.kimanufaktur.nsm.graph.entities.nodes.DoubleNodeWithMultipleThresholds
 import org.jgrapht.Graph
@@ -28,38 +19,13 @@ class ArcMarkerPassing(
         doInitialMarking(listOf(startActivation), this)
     }
 
-    private fun WeightedEdge.toLink() = when (this.edgeType) {
-        EdgeType.Synonym -> SynonymLink()
-        EdgeType.Antonym -> AntonymLink()
-        EdgeType.Hyponym -> HyponymLink()
-        EdgeType.Hypernym -> HypernymLink()
-        EdgeType.Meronym -> MeronymLink()
-        EdgeType.Definition -> DefinitionLink()
-        EdgeType.Syntax -> SyntaxLink()
-        EdgeType.NamedEntity -> NamedEntityLink()
-        EdgeType.SemanticRole -> SemanticRoleLink()
-        else -> null
-    }
-
-    private fun Link.reverseType(edgeType: EdgeType) = when (edgeType) {
-        EdgeType.Hyponym -> HypernymLink().reverse()
-        EdgeType.Hypernym -> HyponymLink().reverse()
-        EdgeType.Definition -> null
-        else -> this.reverse()
-    }
-
-    private fun Link.reverse() = this.apply {
-        this.source = this@reverse.target
-        this.target = this@reverse.source
-    }
-
-
     @Suppress("UNCHECKED_CAST")
     override fun <T : DoubleNodeWithMultipleThresholds?> fillNodes(
         graph: Graph<*, *>?,
         threshold: MutableMap<Concept, Double>?,
         nodeType: Class<T>?) {
-        if (graph == null || threshold == null || nodeType != DoubleNodeWithMultipleThresholds::javaClass) return
+        //TODO: check nodeType
+        if (graph == null || threshold == null) return
         fillNodes(
             graph = graph as Graph<Concept, WeightedEdge>,
             thresholdMap = threshold.toMap()
@@ -71,26 +37,28 @@ class ArcMarkerPassing(
         thresholdMap: Map<Concept, Double>
     ) {
         graph.vertexSet().forEach { sourceConcept ->
-            val source = nodes[sourceConcept]
+            val sourceNode = nodes[sourceConcept]
                 ?: DoubleNodeWithMultipleThresholds(sourceConcept)
                     .also { it.threshold = thresholdMap }
+                    .also { nodes[sourceConcept] = it }
 
-            graph.edgesOf(sourceConcept).forEach { edge ->
-                edge.toLink()?.also { link ->
-                    link.source = source
-                    val targetConcept = edge.target as Concept
-                    val target = nodes[targetConcept]
-                        ?: DoubleNodeWithMultipleThresholds(targetConcept)
-                            .also { it.threshold = thresholdMap }
-                    link.target = target
-                    if (link !in source.links) source.addLink(link)
-                    link.reverseType(edge.edgeType)?.let { reversedLink ->
-                        if (link !in target.links) target.addLink(reversedLink)
+            graph.edgesOf(sourceConcept).filter { edge -> edge.source as Concept == sourceConcept }
+                .forEach { edge ->
+                    edge.toEmptyLink()?.also { link ->
+                        link.source = sourceNode
+                        val targetConcept = edge.target as Concept
+                        val targetNode = nodes[targetConcept]
+                            ?: DoubleNodeWithMultipleThresholds(targetConcept)
+                                .also { it.threshold = thresholdMap }
+                        link.target = targetNode
+                        if (link !in sourceNode.links) sourceNode.addLink(link)
+                        link.reverseByType(edge.edgeType)?.let { reversedLink ->
+                            if (link !in targetNode.links) targetNode.addLink(reversedLink)
+                        }
+                        nodes[sourceConcept] = sourceNode
+                        nodes[targetConcept] = targetNode
                     }
-                    nodes[sourceConcept] = source
-                    nodes[targetConcept] = target
                 }
-            }
         }
     }
 }
