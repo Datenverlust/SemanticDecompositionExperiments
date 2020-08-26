debcfc932c47a064892e60bbddd19f2df2187501
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-08-26 11:23:26 +0200

    add differentiation by wordtype to resolve negation

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/negation/ResolveNegation.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/negation/ResolveNegation.kt
@@ -9,6 +9,12 @@ import arc.wsd.disambiguateBy
 import de.kimanufaktur.nsm.decomposition.Concept
 import edu.stanford.nlp.pipeline.CoreDocument
 
+private val verbTags = listOf("MD", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ")
+private val nounTags = listOf("NN", "NNP", "NNS", "NNPS")
+private val adjectiveTags = listOf("JJ", "JJR", "JJS")
+private val adverbTags = listOf("RB", "RBR", "RBS")
+private val negatedTags = listOf(verbTags, nounTags, adjectiveTags, adverbTags).flatten()
+
 fun Concept.resolveNegation(
     config: ArcGraphConfig,
     markedContext: String
@@ -26,5 +32,33 @@ private fun Concept.pseudoAntonym() = copy().also {
     it.senseKeyToAntonymsMap = senseKeyToSynonymsMap
 }
 
-fun CoreDocument.findNegationTargets() = syntaxEdges().filter { edge -> edge.relation.shortName == "neg" }
-    .map { edge -> edge.source.backingLabel() }
+fun CoreDocument.findNegationTargets() = syntaxEdges().let { edgeList ->
+    edgeList.filter { edge -> edge.relation.shortName == "neg" }
+        .map { edge -> edge.source.backingLabel() }
+        .filter { sourceLabel -> sourceLabel.tag() in negatedTags }
+        .mapNotNull { sourceLabel ->
+            if (sourceLabel.tag() in nounTags) {
+                edgeList
+                    .filter { edge ->
+                        edge.source.backingLabel() == sourceLabel
+                            && edge.target.backingLabel().tag() in negatedTags
+                    }
+                    .filterNot { edge -> edge.target.backingLabel().tag() in nounTags }
+                    .let { relevantEdges ->
+                        relevantEdges.firstOrNull { edge -> edge.relation.shortName == "amod" }
+                            ?: relevantEdges.firstOrNull { edge -> edge.relation.shortName == "vmod" }
+                            ?: relevantEdges.firstOrNull { edge -> edge.relation.shortName == "rcmod" }
+                            ?: relevantEdges.firstOrNull { edge -> edge.relation.shortName == "advmod" }
+                            ?: relevantEdges.firstOrNull { edge -> edge.relation.shortName == "cop" }
+                        //TODO: check negations that could be solved replacement ...
+                        // "no one" - "nobody"
+                        // "no way" - "noway"/"never"
+                    }
+                    ?.target
+                    ?.backingLabel()
+                    .let {
+                        it
+                    }
+            } else sourceLabel
+        }
+}
