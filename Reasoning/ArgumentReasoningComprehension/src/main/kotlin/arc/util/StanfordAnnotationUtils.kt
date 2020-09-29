af3d46c6ecee598e248264690f5b822d186447c7
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-09-29 14:17:22 +0200

    mute stanford annotation

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/StanfordAnnotationUtils.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/StanfordAnnotationUtils.kt
@@ -2,11 +2,13 @@ package arc.util
 
 import edu.stanford.nlp.pipeline.CoreDocument
 import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.util.logging.RedwoodConfiguration
 import java.util.Properties
 
 private val stanfordPipeline = StanfordCoreNLP(
     Properties()
         .also {
+            RedwoodConfiguration.current().clear().apply()
             it.setProperty("annotators", "tokenize, ssplit, pos, lemma,  parse, ner")
             it.setProperty("ner.useSUTime", "false")
             it.setProperty("parse.originalDependencies", "true")
