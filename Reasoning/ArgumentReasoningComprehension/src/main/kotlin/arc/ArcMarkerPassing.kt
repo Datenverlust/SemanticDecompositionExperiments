cda0f525c9552c03e78cb82ddc62f89f4b30ff9b
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-10-01 13:18:49 +0200

    use compressed markerpassing

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcMarkerPassing.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/ArcMarkerPassing.kt
@@ -4,56 +4,56 @@ import arc.util.reverseByType
 import arc.util.toEmptyLink
 import de.kimanufaktur.nsm.decomposition.Concept
 import de.kimanufaktur.nsm.decomposition.graph.edges.WeightedEdge
-import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.DoubleMarkerPassing
 import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.MarkerPassingConfig
-import de.kimanufaktur.nsm.graph.entities.marker.DoubleMarkerWithOrigin
-import de.kimanufaktur.nsm.graph.entities.nodes.DoubleNodeWithMultipleThresholds
+import de.kimanufaktur.nsm.decomposition.graph.spreadingActivation.MarkerPassing.StringDoubleMarkerPassing
+import de.kimanufaktur.nsm.graph.entities.marker.StringDoubleMarkerWithOrigin
+import de.kimanufaktur.nsm.graph.entities.nodes.StringDoubleNodeWithMultipleThresholds
 import org.jgrapht.Graph
 
 var currentConfig: ArcConfig = ArcConfig()
 
 class ArcMarkerPassing(
-    graph: Graph<Concept, WeightedEdge>,
-    threshold: Map<Concept, Double>,
-    nodeType: Class<DoubleNodeWithMultipleThresholds>
-) : DoubleMarkerPassing(graph, threshold, nodeType) {
+    graph: Graph<String, WeightedEdge>,
+    threshold: Map<String, Double>,
+    nodeType: Class<StringDoubleNodeWithMultipleThresholds>
+) : StringDoubleMarkerPassing(graph, threshold, nodeType) {
 
-    fun doInitialMarking(startActivation: Map<Concept, List<DoubleMarkerWithOrigin>>) {
+    fun doInitialMarking(startActivation: Map<String, List<StringDoubleMarkerWithOrigin>>) {
         doInitialMarking(listOf(startActivation), this)
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : DoubleNodeWithMultipleThresholds?> fillNodes(
+    override fun <T : StringDoubleNodeWithMultipleThresholds?> fillNodes(
         graph: Graph<*, *>?,
-        threshold: MutableMap<Concept, Double>?,
+        threshold: MutableMap<String, Double>?,
         nodeType: Class<T>?) {
         //TODO: check nodeType
         MarkerPassingConfig.setDoubleActivationLimit(Double.POSITIVE_INFINITY)
         MarkerPassingConfig.setTerminationPulsCount(99)
         if (graph == null || threshold == null) return
         fillNodes(
-            graph = graph as Graph<Concept, WeightedEdge>,
+            graph = graph as Graph<String, WeightedEdge>,
             thresholdMap = threshold.toMap()
         )
     }
 
     private fun fillNodes(
-        graph: Graph<Concept, WeightedEdge>,
-        thresholdMap: Map<Concept, Double>
+        graph: Graph<String, WeightedEdge>,
+        thresholdMap: Map<String, Double>
     ) {
         graph.vertexSet().forEach { sourceConcept ->
             val sourceNode = nodes[sourceConcept]
-                ?: DoubleNodeWithMultipleThresholds(sourceConcept)
+                ?: StringDoubleNodeWithMultipleThresholds(sourceConcept)
                     .also { it.threshold = thresholdMap }
                     .also { nodes[sourceConcept] = it }
 
-            graph.edgeSet().filter { edge -> edge.source as Concept == sourceConcept }
+            graph.edgeSet().filter { edge -> edge.source as String == sourceConcept }
                 .forEach { edge ->
                     edge.toEmptyLink(currentConfig)?.also { link ->
                         link.source = sourceNode
-                        val targetConcept = edge.target as Concept
+                        val targetConcept = edge.target as String
                         val targetNode = nodes[targetConcept]
-                            ?: DoubleNodeWithMultipleThresholds(targetConcept)
+                            ?: StringDoubleNodeWithMultipleThresholds(targetConcept)
                                 .also { it.threshold = thresholdMap }
                         link.target = targetNode
                         if (link !in sourceNode.links) sourceNode.addLink(link)
