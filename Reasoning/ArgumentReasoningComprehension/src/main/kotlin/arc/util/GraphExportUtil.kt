0ea3b69636032dd7cef6af5d44b623990eb26f11
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-10-01 13:18:07 +0200

    add graph importer

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/GraphExportUtil.kt
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/GraphExportUtil.kt
@@ -1,32 +1,27 @@
 package arc.util
 
-import de.kimanufaktur.nsm.decomposition.Concept
+import de.kimanufaktur.nsm.decomposition.graph.edges.EdgeType
 import de.kimanufaktur.nsm.decomposition.graph.edges.WeightedEdge
 import org.jgrapht.graph.DefaultListenableGraph
-import org.jgrapht.io.AttributeType
-import org.jgrapht.io.DefaultAttribute
 import org.jgrapht.io.GraphMLExporter
+import org.jgrapht.io.GraphMLImporter
 import java.io.File
 
-val exporter = GraphMLExporter<Concept, WeightedEdge>(
-    { concept -> concept.hashCode().toString() },
-    { concept ->
-        concept.litheral + (if (concept.assignedSenseKeys.isNotEmpty()) " : ${concept.assignedSenseKeys}" else "") + (if (concept.negated) " NEG" else "")
-    },
-    { concept ->
-        mapOf(
-            "literal" to DefaultAttribute(concept.litheral, AttributeType.STRING),
-            "lemma" to DefaultAttribute(concept.lemma, AttributeType.STRING),
-            "negated" to DefaultAttribute(concept.negated, AttributeType.BOOLEAN),
-            "assignedSenseKeys" to DefaultAttribute(concept.assignedSenseKeys.toString(), AttributeType.STRING),
-            "decompositionLevel" to DefaultAttribute(concept.decompositionlevel, AttributeType.INT)
-        )
-    },
-    { edge -> edge.hashCode().toString() },
-    { edge -> edge.edgeType.name },
-    { mapOf() }
+val exporter = GraphMLExporter<String, WeightedEdge>(
+    { name -> name },
+    { name -> name},
+    { edge -> edge.hashCode().toString()},
+    { edge -> edge.edgeType.name }
+)
+val importer = GraphMLImporter<String, WeightedEdge>(
+    { name, _ -> name.replace("#","_") },
+    { source, target, _, edgeAttributes ->
+        edgeAttributes.values.first().value.let { type ->
+            createEdge(EdgeType.valueOf(type), source, target)
+        }
+    }
 )
 
-fun DefaultListenableGraph<Concept, WeightedEdge>.saveToFile(file: File) {
+fun DefaultListenableGraph<String, WeightedEdge>.saveToFile(file: File) {
     exporter.exportGraph(this, file.writer())
 }
