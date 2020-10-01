faa7499c80f5f7d1b5007b9df5cc8f5d9227c1da
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-10-01 13:15:31 +0200

    implement caches as rocksdbs

---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/GraphCache.kt
@@ -0,0 +1,51 @@
+package arc
+
+import arc.util.userHome
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.rocksdb.Options
+import org.rocksdb.RocksDB
+import java.io.File
+import java.lang.NullPointerException
+
+interface KeyValueRepository<K, V> {
+    fun save(key: K, value: V)
+    fun find(key: K): V?
+    fun delete(key: K)
+}
+
+class GraphCache : KeyValueRepository<String, GraphData> {
+
+    val dbName = "graph"
+    val dbDir: File = File(userHome("Dokumente"), "$dbName-db").also { it.mkdirs() }
+    val options = Options()
+    val mapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
+
+    init {
+        RocksDB.loadLibrary()
+        options.setCreateIfMissing(true)
+    }
+
+    val database: RocksDB = RocksDB.open(options, dbDir.path)
+
+    fun GraphData.serialize(): ByteArray = mapper.writeValueAsBytes(this)
+
+    fun ByteArray.deserialize(): GraphData = mapper.readValue(this)
+
+    override fun save(key: String, value: GraphData) {
+        database.put(key.toByteArray(), value.serialize())
+    }
+
+    override fun find(key: String): GraphData? {
+        return try {
+            database.get(key.toByteArray()).deserialize()
+        }  catch (_: NullPointerException) {
+            null
+        }
+    }
+
+    override fun delete(key: String) {
+        database.delete(key.toByteArray())
+    }
+}
---
+++ Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/SemanticCache.kt
@@ -0,0 +1,59 @@
+package arc
+
+import arc.util.exporter
+import arc.util.importer
+import arc.util.userHome
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import de.kimanufaktur.nsm.decomposition.graph.edges.WeightedEdge
+import org.apache.commons.lang.ObjectUtils
+import org.jgrapht.graph.DefaultDirectedWeightedGraph
+import org.jgrapht.graph.DefaultListenableGraph
+import org.rocksdb.Options
+import org.rocksdb.RocksDB
+import java.io.File
+import java.io.ObjectInputStream
+import java.io.StringWriter
+import java.lang.NullPointerException
+
+class SemanticGraphCache : KeyValueRepository<String, DefaultListenableGraph<String, WeightedEdge>> {
+
+    val dbName = "semanticGraph"
+    val dbDir: File = File(userHome("Dokumente"), "$dbName-db").also { it.mkdirs() }
+    val options = Options()
+    val mapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
+
+    init {
+        RocksDB.loadLibrary()
+        options.setCreateIfMissing(true)
+    }
+
+    val database: RocksDB = RocksDB.open(options, dbDir.path)
+
+    fun DefaultListenableGraph<String, WeightedEdge>.serialize(): ByteArray = StringWriter()
+        .also { exporter.exportGraph(this, it) }
+        .let { mapper.writeValueAsBytes(it.toString()) }
+
+
+    fun ByteArray.deserialize(): DefaultListenableGraph<String, WeightedEdge> =
+        DefaultListenableGraph(DefaultDirectedWeightedGraph<String, WeightedEdge>(WeightedEdge::class.java)).also { graph ->
+            importer.importGraph(graph, mapper.readValue<String>(this).reader())
+        }
+
+
+    override fun save(key: String, value: DefaultListenableGraph<String, WeightedEdge>) {
+        database.put(key.toByteArray(), value.serialize())
+    }
+
+    override fun find(key: String): DefaultListenableGraph<String, WeightedEdge>? =
+        try {
+            database.get(key.toByteArray()).deserialize()
+        } catch (_: NullPointerException) {
+            null
+        }
+
+    override fun delete(key: String) {
+        database.delete(key.toByteArray())
+    }
+}
