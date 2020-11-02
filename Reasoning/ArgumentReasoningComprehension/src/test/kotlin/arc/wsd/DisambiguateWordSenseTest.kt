1e5c1b6cbf505e07685eabdada92a3ae5a0afee3
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-11-02 08:46:53 +0100

    create wsd test

--- Reasoning/ArgumentReasoningComprehension/src/test/kotlin/arc/wsd/DisambiguateWordSenseTest.kt
+++ Reasoning/ArgumentReasoningComprehension/src/test/kotlin/arc/wsd/DisambiguateWordSenseTest.kt
@@ -1,3 +1,23 @@
 package arc.wsd
 
-//TODO: move wsd Sandbox and transform to test
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Assert
+import org.junit.Test
+
+class DisambiguateWordSenseTest {
+    @Test
+    fun disambiguateTest() {
+        {}::class.java.getResourceAsStream("wsd_examples.yml").readBytes().let { bytes ->
+            ObjectMapper(YAMLFactory()).registerModule(KotlinModule())
+                .readValue<List<WSDRequest>>(bytes)
+                .mapNotNull { request -> request.process()?.let { response -> request to response } }
+                .forEach { (request, senseList) ->
+                    Assert.assertTrue(senseList.all { sense -> sense in request.wordSenses })
+                    Assert.assertTrue(senseList.isNotEmpty())
+                }
+        }
+    }
+}
