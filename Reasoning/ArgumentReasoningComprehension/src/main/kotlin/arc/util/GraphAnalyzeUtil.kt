43ba503748191a97c1876a930e05f1b23de8e2af
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-11-02 08:49:59 +0100

    remove unused scripts

--- Reasoning/ArgumentReasoningComprehension/src/main/kotlin/arc/util/GraphAnalyzeUtil.kt
+++
@@ -1,8 +0,0 @@
-package arc.util
-
-import de.kimanufaktur.nsm.decomposition.graph.edges.WeightedEdge
-import org.jgrapht.graph.DefaultListenableGraph
-
-fun DefaultListenableGraph<String, WeightedEdge>.printSize() {
-    println("#vertices: ${vertexSet().size}\n#edges: ${edgeSet().size}")
-}
